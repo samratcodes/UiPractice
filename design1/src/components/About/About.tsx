28f865ff8496eb74cd776c2ea910eb245d7c91e2
samratcodes <samratcodess@gmail.com>
2023-06-24 15:23:29 +0545

    first design complete

--- design1/src/components/About/About.tsx
+++ design1/src/components/About/About.tsx
@@ -1,10 +1,42 @@
 import './About.scss'
 const About = () => {
   return (
-    <div className='About'>
-        About
+    <div className="About">
+      <div className="About__Left">
+        <div className="About__Left__text">
+          ABOUT <br /> DESIGN WEEK
         </div>
-  )
+        <div className="About__Left__Button">CONFIRM YOUR SEAT &#8594;</div>
+      </div>
+      <div className="About__Right">
+        <div className="About__Right__text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          autem magni esse voluptates, dolor dicta quibusdam nisi cum rerum
+          recusandae adipisci, vel praesentium pariatur odit quod ullam
+          voluptatum. Similique, magnam? Nobis doloremque dolores veritatis fuga
+          quidem labore aut porro deserunt tempore sint soluta laborum quia
+          nisi, maiores ad nam optio suscipit. Nihil vel minima fugiat, ratione
+          quibusdam, beatae nam repellat neque praesentium dolorem saepe optio
+          labore perferendis ipsum molestiae in molestias voluptates?
+          Consequatur adipisci illo ullam tenetur iusto? Enim mollitia tempora
+          placeat, officia maiores, quasi reiciendis possimus dolorem ut
+          praesentium ducimus a iure quaerat iusto et odit! Omnis, odit numquam?
+        </div>
+        <div className="About__Right__Details">
+          <div className="About__Right__Details__text">
+            <div className="About__Right__Details__text__1">03</div>
+            <div className="About__Right__Details__text__2">10+</div>
+            <div className="About__Right__Details__text__3">23+</div>
+          </div>
+          <div className="About__Right__Details__description">
+            <div className="About__Right__Details__description__1">DAY EVENT</div>
+            <div className="About__Right__Details__description__2">DAY EVENT</div>
+            <div className="About__Right__Details__description__3">SPONSER</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
